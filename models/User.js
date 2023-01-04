a7cd7a074d80200277b1b524209ad7486d3e1ba0
Usama Javed <usamajaved1119@gmail.com>
2023-01-04 22:55:38 +0500

    added expenses schema

--- models/User.js
+++ models/User.js
@@ -6,6 +6,12 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  expenses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Expense",
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
